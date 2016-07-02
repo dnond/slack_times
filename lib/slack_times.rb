4db0f3c634ef5b16c18e8bbaab6bb1abb9788747
dnond <tarosuk@gmail.com>
2016-07-02 16:11:59 +0900

    initial commit

---
+++ lib/slack_times.rb
@@ -0,0 +1,30 @@
+require 'slack_times/version'
+require 'slack'
+
+module SlackTimes
+  class Chat
+    def initialize
+      Slack.configure do |config|
+        config.token = ENV['SLACK_TOKEN']
+      end
+    end
+
+    def post(message)
+      return 'no message!' if message.nil?
+      return 'no channel!' if channel.nil?
+      # binding.pry
+      Slack.chat_postMessage post_options.merge(text: message, channel: channel)
+    end
+
+    private
+
+    def channel
+      ENV['SLACK_TIMES_CHANNEL']
+    end
+
+    def post_options
+      # options doc: https://github.com/aki017/slack-ruby-gem/blob/dev/lib/slack/endpoint/chat.rb#L24
+      { as_user: true }
+    end
+  end
+end
---
+++ lib/slack_times/version.rb
@@ -0,0 +1,3 @@
+module SlackTimes
+  VERSION = "0.1.0"
+end
